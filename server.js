4aa3b24294fabd4d136be6141e578313f625c99f
pinar1987 <pinarbildirici87@gmail.com>
2025-03-28 21:47:04 +0100

    get users done

--- server.js
+++ server.js
@@ -4,5 +4,13 @@ app.use(express.json());
 
 const PORT = 3000;
 
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+let users = [
+    { id: 1, userName: "Alice", email: "alice@example.com" },
+    { id: 2, userName: "Bob", email: "bob@example.com" }
+];
+
+app.get('/users', (req, res) => {
+    res.json(users);
+});
